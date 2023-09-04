575380a9ad19cef8998997c8cb84772b4234675a
umar-uzdenov <90197962+umar-uzdenov@users.noreply.github.com>
2023-09-04 07:43:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new Swiper(".slider", {
+    // freeMode: true,
+    speed: 700,
+    direction: "vertical",
+    mousewheel: true,
+    spaceBetween: 18,
+    // parallax: true,
+    loop: true,
+})
